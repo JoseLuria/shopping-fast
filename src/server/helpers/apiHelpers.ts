b11b9d803f8e257137941efe854bc8f125f94a94
JoseLuria <jos02-log18@hotmail.com>
2023-02-26 15:49:40 -0600

    feat: :sparkles: created exclude fields helper

--- src/server/helpers/apiHelpers.ts
+++ src/server/helpers/apiHelpers.ts
@@ -1,3 +1,10 @@
 export function response(status: number, data: any) {
   return new Response(JSON.stringify(data), { status })
 }
+
+export const excludeFields = <T, Key extends keyof T>(model: T, keys: Key[]): Omit<T, Key> => {
+  for (const key of keys) {
+    delete model[key]
+  }
+  return model
+}
