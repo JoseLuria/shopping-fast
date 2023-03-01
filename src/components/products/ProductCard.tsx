d78b8a08f1f4b6a45c7a0728eae7e52e176d501b
JoseLuria <jos02-log18@hotmail.com>
2023-03-01 17:20:12 -0600

    feat: :lipstick: created product card component

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,37 @@
+import type { FunctionComponent } from 'preact'
+import type { ProductResponse } from '@/types'
+import { formatCurrency } from '@/utils'
+
+const ProductCard: FunctionComponent<ProductResponse> = ({
+  name,
+  slug,
+  images,
+  price,
+  oldPrice
+}) => {
+  return (
+    <a title={name} href={`/products/${slug}`}>
+      <picture class='aspect-square rounded-lg bg-white flex'>
+        <img
+          width={1600}
+          height={1280}
+          loading='lazy'
+          class='w-full m-auto'
+          src={images[0]}
+          alt={name}
+        />
+      </picture>
+      <h3 class='text-lg my-2 font-medium leading-5 line-clamp-3'>{name}</h3>
+      <span class='flex mt-auto justify-between flex-wrap items-center gap-2'>
+        <p class='text-white text-opacity-70 text-lg font-medium'>{formatCurrency(price)}</p>
+        {oldPrice && (
+          <span class='h-4 text-sm text-white text-opacity-50 line-through'>
+            {formatCurrency(oldPrice)}
+          </span>
+        )}
+      </span>
+    </a>
+  )
+}
+
+export default ProductCard
